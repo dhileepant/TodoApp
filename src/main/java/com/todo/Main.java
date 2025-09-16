12f98614b29ce282189307328b5cfb87e6d9fef4
dhileepant <dhileepantv@gmail.com>
2025-09-16 14:13:42 +0530

    Database changes

--- src/main/java/com/todo/Main.java
+++ src/main/java/com/todo/Main.java
@@ -1,9 +1,17 @@
-package main.java.com.todo;
+package com.todo;
 
+import com.todo.util.DatabaseConnection;
 public class Main
 {
     public static void main(String args[])
     {
-
+        DatabaseConnection db_Connection = new DatabaseConnection();
+        try {
+            db_Connection.getDBConnection();
+            System.out.println("Database connected successfully.");
+        } catch (Exception e) {
+            System.out.println("Failed to connect to the database.");
+            e.printStackTrace();
+        }
     }
 }
